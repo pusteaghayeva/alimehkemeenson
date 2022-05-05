1636fa770d6b744e17b5b171cd33ddf0af42a3f3
pusteaghayeva <puste.aghayeva@gmail.com>
2022-05-05 10:00:58 +0400

    İç səhifələr, single_news

--- resources/views/pages/singlenews.blade.php
+++ resources/views/pages/singlenews.blade.php
@@ -1,19 +1,26 @@
 @extends('layouts.master')
-{{--@section('title')--}}
-{{--    {{$singlenews[0]->title}}--}}
-{{--@endsection--}}
+@section('title')
+    {{$singlenews[0]->title}}
+@endsection
 @section('content')
 
     <section class="blog-area pt-5 pb-5">
         <div class="container">
-
             <div class="row">
-                @foreach($singlenewss as $singlenews)
+                <div class="col-xl-12 text-center pb-3">
+                    <div class="section-title service-title">
+
+                        <h2>Xəbərlər</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                @foreach($singlenews as $singlenews_item)
                     <div class="colleges single-news ">
-                        <h1 class="coll-title text-center">{{$singlenews->title}}</h1>
+                        <h2 class="coll-title ">{{$singlenews_item->title}}</h2>
                         <div class="container single_text">
-                            <img src="{{asset('storage/'.$singlenews->image)}}" class="coll-image m-auto" alt="">
-                            <p class="coll-text text-justify"> {!! $singlenews->content !!}</p>
+                            <img src="assets/img/news/tedbir 1.jpeg" class="coll-image m-auto" alt="">
+                            <p class="coll-text text-justify"> {!! $singlenews_item->content !!}</p>
                         </div>
                     </div>
                 @endforeach
